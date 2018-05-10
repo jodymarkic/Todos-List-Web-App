39b493b77bc227f458d77e136e2189ddbaf66eff
J M <jody.markic@gmail.com>
2018-05-10 13:15:01 -0400

    added functionality of adding/removing items in the todo list, items can also to be crossed out to indicate they are complete.

--- assests/js/todos.js
+++ assests/js/todos.js
@@ -1,1 +1,23 @@
-alert("Connected!");
+//Check of Specific Todos By Clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+})
+
+//click on x to delete todo
+
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13) { //checking for enter keypress
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
+})
